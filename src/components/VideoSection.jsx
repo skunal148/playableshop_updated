bcd6f409b7509c97f4b025b3e7454242c97dc756
Kunal Shinde <Kunal.Shinde@microscan.co.in>
2025-09-02 09:55:49 +0530

    added videos

--- src/components/VideoSection.jsx
+++ src/components/VideoSection.jsx
@@ -1,10 +1,12 @@
 // src/components/VideoSection.jsx
 import "../styles/VideoSection.css";
+
 export default function VideoSection() {
     return (
       <section className="video-section">
-        <div className="video-placeholder">Video</div>
+        <div className="video">
+        <video src="./video_banner.mp4" autoPlay muted loop playsInline></video>
+        </div>
       </section>
     );
   }
-  
